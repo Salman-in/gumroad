f2efa6ecc21d28819862cd4d3197bea05714c84a
Jono Mingard <reason.koan@gmail.com>
2025-09-13 20:36:33 +0100

    Fix user check

--- app/javascript/components/server-components/Product/index.tsx
+++ app/javascript/components/server-components/Product/index.tsx
@@ -1,19 +1,17 @@
+import cx from "classnames";
 import * as React from "react";
 import { createCast } from "ts-safe-cast";
 
 import { register } from "$app/utils/serverComponentUtil";
 
-import { Layout, Props } from "$app/components/Product/Layout";
-
 import { useLoggedInUser } from "$app/components/LoggedInUser";
-
-import cx from "classnames";
+import { Layout, Props } from "$app/components/Product/Layout";
 
 const ProductPage = (props: Props) => {
   const loggedInUser = useLoggedInUser();
 
   return (
-    <main className={cx("custom-sections", loggedInUser && "has-user")}>
+    <main className={cx("custom-sections", loggedInUser?.id === props.creator_profile.external_id && "has-user")}>
       <Layout {...props} />
       <footer>
         Powered by <span className="logo-full" />
