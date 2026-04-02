d4ed0fa6081367b9a32de91b8ec21f594fc07d67
Salman Najah <salmannajah123@gmail.com>
2026-04-03 00:34:22 +0530

    Fix

--- app/javascript/components/Download/FileList.tsx
+++ app/javascript/components/Download/FileList.tsx
@@ -200,7 +200,7 @@ export const FileRow = ({
     toggleAudioDrawer();
   };
 
-  if (isMobileAppWebView && isEmbed && FileUtils.isAudioExtension(file.extension)) {
+  if (isMobileAppWebView && FileUtils.isAudioExtension(file.extension)) {
     return <MobileAppAudioFileRow file={file} />;
   }
 
